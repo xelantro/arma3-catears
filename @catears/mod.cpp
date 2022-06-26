b8518cc8696d86c6e81c4712a56761847184b746
xelantro <40129767+xelantro@users.noreply.github.com>
2022-06-27 00:30:31 +0200

    Build Version

---
+++ @catears/mod.cpp
@@ -0,0 +1,11 @@
+name = "Cat Ears!";
+picture = "preview.paa";
+hidePicture = 0;
+hideName = 0;
+author = "Xelantro";
+actionName = "Website";
+action = "https://gamerparty.eu";
+description = "Adds Cat Ear NVGs";
+logo = "\catears\icon\icon_pink.paa";
+logoOver = "\catears\icon\icon_olive.paa";
+overview = "Cat Ears as NVGs :3 Avaliable in multiple colors and heights!";
